d743c0999ec9500a4e42e03a4ac4d37a7ad7f048
rafia723 <rafiaafzal723@gmail.com>
2024-03-31 17:09:44 +0500

    updated

--- AssignedCourses.js
+++ AssignedCourses.js
@@ -12,7 +12,7 @@ AssignedCoursesRouter.get("/getAssignedCourses/:f_id", (req, res) => {
   }
   // SQL query to retrieve assigned courses for a faculty by ID
   const getAssignedCoursesQuery = `
-    SELECT ac.ac_id, f.f_name AS TeacherName, c.c_title AS CourseTitle, c.c_code AS CourseCode
+    SELECT ac.*, f.f_name , c.c_title , c.c_code 
     FROM faculty f
     JOIN Assigned_Course ac ON f.f_id = ac.f_id
     JOIN course c ON ac.c_id = c.c_id
@@ -37,7 +37,7 @@ AssignedCoursesRouter.get("/getAssignedTo/:c_id", (req, res) => {
   }
   // SQL query to retrieve faculty assigned to a course by ID
   const getAssignedToQuery = `
-    SELECT ac.ac_id, ac.c_id, ac.f_id, f.f_name AS TeacherName, c.c_title AS CourseTitle, c.c_code AS CourseCode, ac.role
+    SELECT ac.*, f.f_name, c.c_title , c.c_code,
     FROM faculty f
     JOIN Assigned_Course ac ON f.f_id = ac.f_id
     JOIN course c ON ac.c_id = c.c_id
@@ -54,63 +54,7 @@ AssignedCoursesRouter.get("/getAssignedTo/:c_id", (req, res) => {
   });
 });
 
-// POST endpoint
-AssignedCoursesRouter.post("/addAssignedCourse", (req, res) => {
-  const { c_id, f_id } = req.body;
-  const role = "Junior";
-  console.log("Data received:", { c_id, f_id, role });
-  // SQL query to insert an assigned course
-  const postQuery = "INSERT INTO Assigned_Course (c_id, f_id, role) VALUES (?, ?, ?)";
-  pool.query(postQuery, [c_id, f_id, role], (error, results) => {
-    if (error) {
-      console.error("Error inserting data:", error);
-      return res.status(500).json({ error: "Post Request Error" });
-    }
-    res.status(200).json({ message: "Assigned_Course inserted successfully" });
-  });
-});
-
-//DELETE endpoint
-AssignedCoursesRouter.delete("/deleteAssignedCourse/:ac_id", (req, res) => {
-  const ac_id = req.params.ac_id;
-  if (!/^\d+$/.test(ac_id)) {
-    return res.status(400).json({ error: "Invalid assigned_course ID" });
-  }
-  // SQL query to delete an assigned course
-  const deleteAssignedCourseQuery = "DELETE FROM Assigned_Course WHERE ac_id = ?";
-  pool.query(deleteAssignedCourseQuery, [ac_id], (error, result) => {
-    if (error) {
-      console.error("Error deleting assigned course:", error);
-      return res.status(500).json({ error: "Delete Request Error" });
-    }
-    if (result.affectedRows === 0) {
-      return res.status(404).json({ error: "Assigned_Course not found" });
-    }
-    res.status(200).json({ message: "Assigned_Course deleted successfully" });
-  });
-});
-
-// EDIT STATUS endpoint
-AssignedCoursesRouter.put("/editRole/:c_id/:f_id", async (req, res) => {
-    const c_id = req.params.c_id;
-    const f_id = req.params.f_id;
-    if (!/^\d+$/.test(c_id)) {
-      return res.status(400).json({ error: "Invalid course ID" });
-    }
-    const editStatusQuery = 'UPDATE Assigned_Course SET role = CASE WHEN f_id = ? THEN "Senior" ELSE "Normal" END WHERE c_id = ?';
-    pool.query(editStatusQuery, [f_id, c_id], (error, results) => {
-      if (error) {
-        console.error("Error updating Role:", error);
-        return res.status(500).json({ error: "Edit Role Request Error" });
-      }
-      if (results.affectedRows === 0) {
-        return res.status(404).json({ error: "Assigned Course not found" });
-      }
-      res.status(200).json({ success: "Role updated successfully" });
-    });
-});
-
-
+// GET endpoint
 AssignedCoursesRouter.get("/getUnassignedCourses/:f_id", async (req, res) => {
   try {
     const userId = req.params.f_id;
@@ -137,6 +81,7 @@ AssignedCoursesRouter.get("/getUnassignedCourses/:f_id", async (req, res) => {
   }
 });
 
+// POST endpoint
 AssignedCoursesRouter.post("/assignCourse/:c_id/:f_id", (req, res) => {
   const c_id = req.params.c_id;
   if (!/^\d+$/.test(c_id)) {
@@ -161,5 +106,44 @@ AssignedCoursesRouter.post("/assignCourse/:c_id/:f_id", (req, res) => {
   });
 });
 
+// EDIT STATUS endpoint
+AssignedCoursesRouter.put("/editRole/:c_id/:f_id", async (req, res) => {
+    const c_id = req.params.c_id;
+    const f_id = req.params.f_id;
+    if (!/^\d+$/.test(c_id)) {
+      return res.status(400).json({ error: "Invalid course ID" });
+    }
+    const editStatusQuery = 'UPDATE Assigned_Course SET role = CASE WHEN f_id = ? THEN "Senior" ELSE "Normal" END WHERE c_id = ?';
+    pool.query(editStatusQuery, [f_id, c_id], (error, results) => {
+      if (error) {
+        console.error("Error updating Role:", error);
+        return res.status(500).json({ error: "Edit Role Request Error" });
+      }
+      if (results.affectedRows === 0) {
+        return res.status(404).json({ error: "Assigned Course not found" });
+      }
+      res.status(200).json({ success: "Role updated successfully" });
+    });
+});
+
+//DELETE endpoint
+AssignedCoursesRouter.delete("/deleteAssignedCourse/:ac_id", (req, res) => {
+  const ac_id = req.params.ac_id;
+  if (!/^\d+$/.test(ac_id)) {
+    return res.status(400).json({ error: "Invalid assigned_course ID" });
+  }
+  // SQL query to delete an assigned course
+  const deleteAssignedCourseQuery = "DELETE FROM Assigned_Course WHERE ac_id = ?";
+  pool.query(deleteAssignedCourseQuery, [ac_id], (error, result) => {
+    if (error) {
+      console.error("Error deleting assigned course:", error);
+      return res.status(500).json({ error: "Delete Request Error" });
+    }
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ error: "Assigned_Course not found" });
+    }
+    res.status(200).json({ message: "Assigned_Course deleted successfully" });
+  });
+});
 
 module.exports = AssignedCoursesRouter;
