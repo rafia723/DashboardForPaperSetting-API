b5f27c5bd2e23f1c773b4ac773a28a9c8b9c74a1
rafia723 <rafiaafzal723@gmail.com>
2024-04-01 09:04:28 +0500

    updated

--- Paper.js
+++ Paper.js
@@ -60,4 +60,34 @@ paperRouter.get('/SearchApprovedPapers', async (req, res) => {
     }
 });
 
+paperRouter.get('/getUploadedPapers', async (req, res) => {
+ 
+  const query = "SELECT DISTINCT c.c_title,c.c_code, p.status FROM Course c JOIN Paper p ON c.c_id = p.c_id WHERE p.status = 'Uploaded'";
+  pool.query(query, (error, results) => {
+    if (error) {
+      console.error('Error fetching uploaded papers:', error);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+    res.status(200).json(results);
+  });
+});
+
+paperRouter.get('/SearchUploadedPapers', async (req, res) => {
+
+  const { courseTitle } = req.query;
+  const query = "SELECT DISTINCT c.c_title, c.c_code, p.status FROM Course c JOIN Paper p ON c.c_id = p.c_id WHERE p.status = 'Uploaded'";
+  
+  if (courseTitle) {
+    pool.query(query + " AND (c.c_title LIKE ? OR c.c_code LIKE ?)", [`%${courseTitle}%`, `%${courseTitle}%`], (error, results) => {
+      if (error) {
+        console.error('Error fetching uploaded papers:', error);
+        return res.status(500).json({ error: 'Internal Server Error' });
+      }
+      res.status(200).json(results);
+    });
+  } else {
+    return res.status(400).json({ error: 'Missing courseTitle parameter' });
+  }
+});
+
 module.exports = paperRouter;
