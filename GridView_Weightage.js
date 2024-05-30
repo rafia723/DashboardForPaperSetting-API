2d7bb4ca3df88edccd09e60a739ad6fcb572f066
rafia723 <rafiaafzal723@gmail.com>
2024-05-30 06:40:53 +0500

    updated

--- GridView_Weightage.js
+++ GridView_Weightage.js
@@ -3,10 +3,10 @@ const { pool } = require("./database");
 
 const gridviewWeightageRouter = express.Router();
 
-gridviewWeightageRouter.get('/getGridViewWeightage', async (req, res) => {
- 
-    const query = "SELECT * from GridView_Weightage";
-    pool.query(query, (error, results) => {
+gridviewWeightageRouter.get('/getGridViewWeightage/:clo_id', async (req, res) => {
+  const clo_id = req.params.clo_id;
+    const query = "SELECT * from Grid_View_Weightage where clo_id=?";
+    pool.query(query,clo_id, (error, results) => {
       if (error) {
         console.error('Error fetching weightage :', error);
         return res.status(500).json({ error: 'Internal Server Error' });
@@ -15,6 +15,22 @@ gridviewWeightageRouter.get('/getGridViewWeightage', async (req, res) => {
     });
   });
 
+
+  // To Get Grid_View_Weightage of Course
+gridviewWeightageRouter.get("/getCourseGridViewWeightage/:c_id", (req, res) => {
+  const c_id = req.params.c_id;
+  const getQuery =
+  ` SELECT gvwt.*, clo.c_id FROM Course JOIN clo ON Course.c_id = CLO.c_id JOIN Grid_View_Weightage gvwt 
+    ON CLO.clo_id = gvwt.clo_id WHERE Course.c_id = ?; ` ;
+  pool.query(getQuery, c_id, (err, results) => {
+    if (err) {
+      console.error("Error executing the query:", err);
+      res.status(500).json({ error: "Internal Server Error" });
+      return;
+    }
+    res.json(results);
+  });
+});
 
   gridviewWeightageRouter.post("/addGridViewWeightage", async (req, res) => {
     const { clo_id,header_id,weightage } = req.body;
